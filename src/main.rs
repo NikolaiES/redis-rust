ff91f077280701808444161b75fcadc170e0f8d1
Nikolai Eugen Sandvik <nikolai+git@sandvik.me>
2023-07-06 19:36:17 +0200

    Ping

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,8 +8,9 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let _ = _stream.write(b"+PONG\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
