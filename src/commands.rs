4045af6eb837a279c78833bf4dfa334cca1a2765
Nikolai Eugen Sandvik <nikolai-git@sandvik.dev>
2023-07-13 16:07:21 +0200

    Refactor handle_set function and add support for additional command options

--- src/commands.rs
+++ src/commands.rs
@@ -55,7 +55,13 @@ pub async fn handle_set(
             client.write_all(b"-ERR syntax error\r\n").await?;
             return Ok(());
         }
+        let mut expiry: Option<Duration> = None;
+        let mut only_if_new = false;
+        let mut only_if_exists = false;
         for (index, command) in commands.iter().enumerate() {
+            if index < 3 {
+                continue
+            }
             if skip_next {
                 skip_next = false;
                 continue;
@@ -63,43 +69,43 @@ pub async fn handle_set(
             match command {
                 &"px" => {
                     skip_next = true;
-                    todo!()
-                }
+                    expiry = Some(Duration::from_millis(commands[index+1].parse::<u64>().unwrap()));
+                },
                 &"ex" => {
                     skip_next = true;
-                    todo!()
+                    expiry = Some(Duration::from_secs(commands[index+1].parse::<u64>().unwrap()))
+                },
+                &"nx" => {
+                    only_if_new = true;
+                },
+                &"xx" => {
+                    only_if_exists = true;
                 }
                 _ => {
                     client.write_all(b"-ERR syntax error\r\n").await?;
+                    return Ok(())
                 }
             }
         }
-        if commands.len() == 5 {
-            if commands[3] == "px" {
-                value = ValueWithExpiry {
-                    value: commands[2].to_string(),
-                    insert_time: Instant::now(),
-                    expiry: Some(Duration::from_millis(commands[4].parse::<u64>().unwrap())),
-                };
-            } else if commands[3] == "ex" {
-                value = ValueWithExpiry {
-                    value: commands[2].to_string(),
-                    insert_time: Instant::now(),
-                    expiry: Some(Duration::from_secs(commands[4].parse::<u64>().unwrap())),
-                }
-            } else {
-                client
-                    .write_all(b"-ERR wrong argument for 'SET' command\r\n")
-                    .await?;
-                return Err(anyhow::anyhow!("wrong argument for 'SET' command"));
-            }
-        } else {
-            value = ValueWithExpiry {
-                value: commands[2].to_string(),
-                insert_time: Instant::now(),
-                expiry: None,
+        {
+            let data = {
+                let data = state.lock().unwrap();
+                data.get(commands[1]).cloned()
             };
+            if only_if_exists && data.is_none() {
+                client.write_all(b"$-1\r\n").await?;
+                return Ok(());
+            }
+            if only_if_new && data.is_some() {
+                client.write_all(b"$-1\r\n").await?;
+                return Ok(());
+            }
         }
+        value = ValueWithExpiry {
+            value: commands[2].to_string(),
+            insert_time: Instant::now(),
+            expiry,
+        };
         let mut data = state.lock().unwrap();
         println!("Inserting data {:?}", value);
         data.insert(commands[1].to_string(), value);
@@ -111,20 +117,46 @@ pub async fn handle_set(
 fn check_set_command_syntax(commands: &Vec<&str>) -> bool {
     let mut px_set = false;
     let mut ex_set = false;
+    let mut xx_set = false;
+    let mut nx_set = false;
 
     for i in 0..commands.len() {
         match commands[i] {
             "px" => {
+                if i < 3 || i +1  == commands.len()  {
+                    return false;
+                }
                 if ex_set {
                     return false;
                 };
                 px_set = true;
             }
             "ex" => {
+                if i < 3 || i +1  == commands.len() {
+                    return false;
+                }
                 if px_set {
                     return false;
                 }
                 ex_set = true;
+            }
+            "xx" => {
+                if i < 3 {
+                    return false;
+                }
+                if nx_set {
+                    return false;
+                }
+                xx_set = true;
+            }
+            "nx" => {
+                if i < 3 {
+                    return false;
+                }
+                if xx_set {
+                    return false;
+                }
+                nx_set = true;
             }
             _ => continue,
         }
